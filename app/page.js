c9bf246c4ee64782e686a8a03478a90ad28213f9
= <=>
2023-08-11 11:16:46 +0300

    Working

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>     
         <p>Hello. This is next js. </p>    
+        <h3>So it's working, right?</h3>
     </main>
   )
 }
