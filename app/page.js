905d198a882d99b8aa576f1b326766c5623a69b9
= <=>
2023-08-11 11:13:54 +0300

    Little changes in the main file

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>Hello. This is next js. </p>
-      </div>
+    <main className={styles.main}>     
+        <p>Hello. This is next js. </p>    
     </main>
   )
 }
