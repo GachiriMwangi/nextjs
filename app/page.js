b1d2b398c1ee7553a3a0b674bc21c1e968870334
Gachiri Mwangi <103571707+GachiriMwangi@users.noreply.github.com>
2023-08-11 11:32:31 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <main className={styles.main}>     
         <p>Hello. This is next js. </p>    
         <h3>So it's working, right?</h3>
+    <h5>Perfect.</h5>
     </main>
   )
 }
